dd6348ef4660436822f840cd57497b78cdb9dfee
Yosuke Mizutani <mogproj@gmail.com>
2014-06-25 00:42:46 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+docker-attach
+====
+
+see 
+http://mogproject.blogspot.jp/2014/06/docker-how-to-run-in-boot2docker-vm.html
+
---
+++ bootlocal.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+ln -s /var/lib/boot2docker/bin/nsinit /usr/local/bin/nsinit
+ln -s /var/lib/boot2docker/bin/docker-attach /usr/local/bin/docker-attach

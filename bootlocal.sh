c5fdbea96870c97bed5f57d4c19388d06efee9ab
Yosuke Mizutani <mogproj@gmail.com>
2014-07-02 01:00:50 +0900

    nsenter version

--- bootlocal.sh
+++ bootlocal.sh
@@ -1,3 +1,23 @@
 #!/bin/sh
-ln -s /var/lib/boot2docker/bin/nsinit /usr/local/bin/nsinit
-ln -s /var/lib/boot2docker/bin/docker-attach /usr/local/bin/docker-attach
+
+B2D_ROOT=/var/lib/boot2docker
+B2D_BIN_DIR=$B2D_ROOT/bin
+
+mkdir -p $B2D_BIN_DIR
+
+# install util-linux
+UTIL_LINUX=$B2D_ROOT/optional/util-linux.tcz
+if [ ! -e $UTIL_LINUX ]; then
+    su - docker -c "tce-load -w util-linux"
+    mv -f /tmp/tce/optional $B2D_ROOT/
+fi
+su - docker -c "tce-load -i $UTIL_LINUX"
+
+# install docker-attach
+URL_DOCKER_ATTACH=https://raw.githubusercontent.com/mogproject/docker-attach/master/docker-attach
+DOCKER_ATTACH=$B2D_BIN_DIR/docker-attach
+if [ ! -x $DOCKER_ATTACH ]; then 
+    curl -s -o $DOCKER_ATTACH $URL_DOCKER_ATTACH
+    chmod +x $DOCKER_ATTACH
+fi
+ln -s $DOCKER_ATTACH /usr/local/bin/docker-attach
